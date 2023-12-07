6511957e831b0277c2ae0cb5150c8453ac1ebb20
KareendraJ <110087765+KareendraJ@users.noreply.github.com>
2023-12-07 15:05:49 +0530

    Update Aparatus.py
    hi I have update the cide for a best JPG

--- Aparatus.py
+++ Aparatus.py
@@ -1,8 +1,10 @@
+# program to capture single image from webcam in python
+import cv2
+# importing OpenCV library
+
 from matplotlib import pyplot as plt
 
 # initialize the camera
-# If you have multiple camera connected with
-# current device, assign a value in cam_port
 # variable according to that
 
 cam = cv2.VideoCapture(0)
@@ -17,3 +19,29 @@ print(image)
 
 plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 plt.show()
+
+cam.release()
+def take_photo():
+    cap = cv2.VideoCapture(3)
+    ret, frame = cap.read()
+    cv2.imwrite('webcamphoto.jpg', frame)
+    cap.release()
+take_photo()
+
+# Connect to webcam
+cap = cv2.VideoCapture(3)
+# Loop through every frame until we close our webcam
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    # Show image
+    cv2.imshow('Webcam', frame)
+
+    # Checks whether q has been hit and stops the loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Releases the webcam
+cap.release()
+# Closes the frame
+cv2.destroyAllWindows()
