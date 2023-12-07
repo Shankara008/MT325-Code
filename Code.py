541726e5a77aeffe4edc5ff85f8bcf406d3bf1c5
Shankara Devapriya <86051434+Shankara008@users.noreply.github.com>
2023-12-08 00:16:43 +0530

    New Code to webcam

--- Code.py
+++ Code.py
@@ -1,31 +1,140 @@
+import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
-# Reading the grayscale image
-img = cv2.imread(r'Fabric/dots7.jpg', 0)
+# Load the image 
+#image = cv2.imread('Fabric/dots7.jpg', cv2.IMREAD_GRAYSCALE)
+
+cam = cv2.VideoCapture(0)
+
+# reading the input using the camera
+result, image = cam.read()
+image_1 = image.copy()
+
+# Convert the image to grayscale
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Apply Gaussian blur for noise reduction
+image_blurred = cv2.GaussianBlur(gray_image, (5,5),0)
 
 # Define the lower and upper bounds for the intensity range
-lower_bound = 50  # Adjust this value for your specific intensity range
-upper_bound = 200  # Adjust this value for your specific intensity range
+lower_bound = 0  # Adjust this value for your specific intensity range
+upper_bound = 100  # Adjust this value for your specific intensity range
 
 # Create a binary mask based on the intensity range
-binary_mask = np.logical_and(img >= lower_bound, img <= upper_bound).astype(np.uint8) * 255
+binary_mask = np.logical_and(image >= lower_bound, image <= upper_bound).astype(np.uint8) * 255
 
-# Convert the binary mask to binary image using thresholding
-_, binary_image = cv2.threshold(binary_mask, 1, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+# Threshold the image to create a binary image #127
+#_, binary_image = cv2.threshold(binary_mask, 127, 255, cv2.THRESH_BINARY)
+binary_mask_gray = cv2.cvtColor(binary_mask, cv2.COLOR_BGR2GRAY)
+_, binary_image = cv2.threshold(binary_mask_gray, 127, 255, cv2.THRESH_BINARY)
 
-# Displaying original grayscale image, binary mask, and binary image
-plt.subplot(131)
-plt.imshow(img, cmap='gray')
+# Find contours in the binary image
+contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Filter contours based on size or other criteria
+min_area = 0
+max_area = 300
+# Adjust this value based on your requirements
+filtered_contours = [contour for contour in contours if max_area > cv2.contourArea(contour) > min_area]
+
+# Calculate centroids of filtered contours
+centroids = [np.mean(contour, axis=0)[0] for contour in filtered_contours]
+
+# Convert centroids to float32
+centroids = np.float32(centroids)
+
+# Draw contours on the original image
+image_with_contours = image.copy()
+cv2.drawContours(image_with_contours, contours, -1, (0, 255, 0), 2)
+
+# Calculate convex hull to order centroids correctly
+hull = cv2.convexHull(np.array(centroids), clockwise=True, returnPoints=True)
+hull = np.int32(hull)
+
+# Create an empty black image
+polygon_image = np.zeros_like(image)
+
+# Draw the convex hull polygon
+cv2.polylines(polygon_image, [hull], isClosed=True, color=255, thickness=2)
+
+# Calculate the area of the polygon
+polygon_area = cv2.contourArea(hull)
+
+distances_list = []
+# Calculate distances between centroids and print them
+for i in range(len(centroids)):
+    for j in range(i + 1, len(centroids)):
+        distance = np.linalg.norm(centroids[i] - centroids[j])
+        distances_list.append([f'Dot {i+1}', f'Dot {j+1}', distance])
+        print(f'Distance between Dot {i+1} and Dot {j+1}: {distance:.2f} pixels')
+
+# Image resize function
+def reSize(frame,scale = 0.25):
+    width = int(frame.shape[1]*scale)
+    height = int(frame.shape[0]*scale)
+
+    dimensions = (width,height)
+    return cv2.resize(frame,dimensions,interpolation=cv2.INTER_AREA)
+
+
+# Convert centroids to integer coordinates
+centroids2 = np.int32(centroids)
+# Function to Calculate the area of the polygon by coordinates.
+def calculate_Area(centroids2):
+    width = abs((centroids2[2][0])-(centroids2[0][0]))
+    Height = abs((centroids2[2][1])-(centroids2[7][1]))
+    polygon_area = width*Height
+    return polygon_area
+
+# Print the area of the polygon
+#print(f"Area of the polygon: {calculate_Area(centroids2)} square pixels")
+
+# Print the area of the polygon
+print(f"Area of the polygon: {polygon_area} square pixels")
+
+# Include the area in the distances list
+distances_list.append(['Polygon Area', '', polygon_area])
+
+# Convert the distances list to a DataFrame
+distances_df = pd.DataFrame(distances_list, columns=['Dot 1', 'Dot 2', 'Distance'])
+
+# Prompt the user to input the sheet name(Roll number)
+sheet_name = input("Enter the Roll Number: ")
+
+# Check if the Excel file exists
+excel_file_path = 'distances.xlsx'
+if not os.path.exists(excel_file_path):
+    # If the file doesn't exist, create a new Excel file
+    with pd.ExcelWriter(excel_file_path, engine='openpyxl', mode='w') as writer:
+        distances_df = pd.DataFrame(distances_list, columns=['Dot 1', 'Dot 2', 'Distance'])
+        distances_df.to_excel(writer, index=False, sheet_name=sheet_name)
+else:
+    # If the file exists, append to the existing file
+    with pd.ExcelWriter(excel_file_path, engine='openpyxl', mode='a') as writer:
+        distances_df = pd.DataFrame(distances_list, columns=['Dot 1', 'Dot 2', 'Distance'])
+        distances_df.to_excel(writer, index=False, sheet_name=sheet_name)
+
+#Showing the original image and processed image...
+plt.subplot(221)
+plt.imshow(image,'gray')
 plt.title("Original")
 
-plt.subplot(132)
-plt.imshow(binary_mask, cmap='gray')
-plt.title("Binary Mask")
-
-plt.subplot(133)
-plt.imshow(binary_image, cmap='gray')
+# Display the binary image with contours
+plt.subplot(222)
+plt.imshow(binary_image, 'gray')
 plt.title("Binary Image")
+
+# Display the original image with contours
+plt.subplot(223)
+plt.imshow(cv2.cvtColor(image_with_contours, cv2.COLOR_BGR2RGB))
+plt.title("Original Image with Contours")
+
+plt.subplot(224)
+plt.title("Processed")
+plt.contour(binary_image,levels=[2],colors='lime')
+plt.imshow(polygon_image)
 
 plt.show()
